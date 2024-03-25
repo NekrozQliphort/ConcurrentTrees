4f05149712df92ebfe1636015e282ef073827531
Ryan Chung <timelord0830@gmail.com>
2024-03-26 06:16:36 +0800

    Add coarse grain locking BBST

---
+++ src/CGLBBST/CGLBBST.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <shared_mutex>
+#include <mutex>
+#include <set>
+
+template <typename T>
+struct CGLBBST {
+  std::set<T> tree;
+  std::shared_mutex mut {};
+
+  bool operator[](const T& key) {
+    std::shared_lock lk{mut};
+    return tree.contains(key);
+  }
+
+  bool insert(const T& key) {
+    std::unique_lock lk{mut};
+    if (tree.contains(key)) return false;
+    tree.insert(key);
+    return true;
+  }
+
+  bool remove(const T& key) {
+    std::unique_lock lk{mut};
+    if (!tree.contains(key)) return false;
+    tree.erase(key);
+    return true;
+  }
+};
