4dc939f9ffda2fb8f749a23c2beba613560ce488
Ryan Chung <timelord0830@gmail.com>
2024-04-05 05:46:58 +0800

    Prepare to run on slurm node

--- job.sh
+++ job.sh
@@ -6,5 +6,7 @@
 #SBATCH --partition="medium"
 #SBATCH --constraint="xcnf"
 
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --target all -j 6
 export BENCHMARK_BUILD="build/BenchmarkBSTImbalanced"
 ./${BENCHMARK_BUILD}
